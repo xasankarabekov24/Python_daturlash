6abe93653fee5fc3804e86a75d6b027bc7a0e1dc
Xasan Karabekov <90567160+xasankarabekov24@users.noreply.github.com>
2022-10-24 21:59:40 +0500

    1_dars_Javoblari

---
+++ 1_dars_javoblari.py
@@ -0,0 +1,19 @@
+#Xasan_Karabekov 24.10.2022 yil
+
+         #Javoblar
+
+#Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+
+#"Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+
+#5 ning 4-darajasini toping
+print('5 ning 4-darajasi', 5**4)
+
+
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22 ni 4 ga bo'lganda qancha qoldiq", 22%4)
+
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng")
