a7c4ab38dc51b1b70a84203b90d7382dd16c6044
Xasan Karabekov <90567160+xasankarabekov24@users.noreply.github.com>
2022-10-25 21:24:39 +0500

    2_dars
    O`zgaruvchilar

---
+++ 2_dars.py
@@ -0,0 +1,85 @@
+# Xasan Karabekov 25.10.2022
+
+# O`ZGARUVCHILAR
+
+
+
+#O`garuvchi- kompyuter xotirasida ma`lum bir qiymatni saqlash uchun ajratilgan joy.
+#O`zgaruvchini quti, quti ichidagi narsani esa qiymat deb tassavur qilish mumkin.
+#Pythonda qiymatlar son, matn, ro`yxat va hakozo ko`rinishida bo`lishi mumkin.
+
+ism = "Xasan" #Natija: Xasan , 30: Biz 2 ta o`zgaruvchi yaratdik (ism va yosh) va ularga qiymatlar yukladik
+yosh = 30     # qiymatlar bu("Xasan" va 30)
+print(ism)    #Pythonda boshqa tillardagi kabi o`zgaruvchilarni oldindan 
+print(yosh)   # elon qilish yo`q
+              
+
+
+
+#O`zgaruvchi deyilishiga sabab uning qiymati istalgan vaqt o`zgartirilishi mumkin
+
+ism = "Xasan" #Natija: Xasan, Muhammad
+print(ism)    # Ushbu misolda o`zgaruvchi avval Xasan keyin esa Muhammad
+ism = "Muhammad" # qiymatlarini yukladik.
+print(ism)
+
+familiya='Karabekov'
+ism='Xasan'
+tugilgan_yili=1992
+print(familiya,ism,tugilgan_yili)
+
+#Ma’lumotlar ekranga chiqqanida ma’lumotning oxiriga nuqta, vergul, probel va h.k. belgilarni chiqarish uchun
+#print() ning end xususiyatidan foydalanamiz:
+
+familiya='Karabekov'
+ism='Xasan'
+tugilgan_yili=1992
+print(familiya,ism,tugilgan_yili,end='.')
+
+
+         #O`ZGARUVCHILARNI NOMLASH
+
+#O`zgaruvchilarni nomlashda quyidagi qoidalarga amal qiling:
+
+#1. O`zgaruvchi nomini harf yoki pastki chiziq bilan boshlanishi kerak
+#2. O`zgaruvchi nomini raqam bilan boshlash mumkin emas
+#3. O`zgaruvchi nomida faqat lotin alifbosi (A,Z), raqamlar (0-9)va
+# pastki chiziq(_) qatnashishi mumkin.
+#4. O`zgaruvchi nomida bo`shliq (probel) bo`lishi mumkin.
+#5. O`zgaruvchi nomida katta-kichik harflar turlicha talqin qilinadi.
+#Masalan(ism,ISM, Ism) uchta turli o`zgaruvchi.
+
+#QO`SHIMCHA QOIDA SIFATIDA :
+
+#1.O`zgaruvchi nomini kichik harflar bilan yozing.
+#2. O`zgaruvchi nomida 2 va undan ortiq so`z ishlatilsa ular orasini pastki
+#chiziq(_) bilan ajrating (ism_sharif = "Xasan Karabekov")
+#3. O`zgaruvchiga tushunarli nom bering(y = 30 emas yosh = 30, d = O`zbekiston
+# emas davlat = O`zbekiston va hakozo)
+#4. O`zgaruvchilarga Pythonda ishlatiladiganfunksiyalar va maxsus kalit so`zlarning
+# (keywords)  nomini bermang
+#Kalit so`zlar ro`yxatini ko`rish uchun Python Shellda avval
+#>>help() deb yozing va enter tugmasni bosing
+#Keyin esa keywords deb kiritib yana Enter tugmasini bosing ekarangizda maxsus kalit sozlar ro`yxatii chiqadi
+#Ushbu ro`yxatdagi kalit so`zlardan o`zgaruvchi sifatida foydalanib bo`lmaydi.
+
+
+#AMALIYOT
+#Quyidagi mashqlarni bajaring:
+
+#"Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+#xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga
+#yangi qiymat berib uni ham konsolga chiqaring.
+#PYTHON da o‘zingizning “Familiya va Ismingizni” ekranga chiqaring.
+#PYTHON da o‘zingizning “Yo’nalishingizni” ekranga chiqaring.
+#PYTHON da o‘zingizning “Universitetingizni nomini” ekranga chiqaring.
+#PYTHON da o‘zingizning “Fakultetingiz nomini” ekranga chiqaring.
+#PYTHON da o‘zingizning “Guruhingiz nomini” ekranga chiqaring.
+#PYTHON da ushbu gapni “O’zbekiston kelajagi buyuk davlat!” ekranga chiqaring.
+#PYTHON da ushbu gapni “Men PYTHON dasturlash tilini o‘rganmoq- chiman” ekranga chiqaring.
+#PYTHON da O‘zbekinton Respublikasi madhiyasining birinchi to‘rtligini ekranga chiqaring.
+#PYTHON da O‘zbekinton Respubliksi madhiyasining ikkinchi to‘rtligini ekranga chiqaring.
+#PYTHON da O‘zbekinton Respubliksi madhiyasining uchinchi to‘rtligini
+
+   #ushbu kodlarni mustaqil bajarishga harakat qiling 
+
---
+++ 2_dars_javoblari.py
@@ -0,0 +1,63 @@
+# Xasan Karabekov 25.10.2022
+#1 "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+matn  = "Hello World"
+print(matn)
+
+#2xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga
+#yangi qiymat berib uni ham konsolga chiqaring.
+xabar = "Asslomu alaykum"
+print(xabar)
+xabar = "xayrli tong"
+print(xabar)
+
+#3 PYTHON da o‘zingizning “Familiya va Ismingizni” ekranga chiqaring.
+familiya = "Karabekov"
+ism = "Xasan"
+#4 PYTHON da o‘zingizning “Yo’nalishingizni” ekranga chiqarin
+yonalish = "Python dasturlash"
+print(yonalish)
+#5 PYTHON da o‘zingizning “Universitetingizni nomini” ekranga chiqaring.
+oliy_talim = "YODJU"
+print(oliy_talim)
+
+#6 PYTHON da o‘zingizning “Fakultetingiz nomini” ekranga chiqaring.
+fakultet = "Boshlang`ich ta`lim"
+print(fakultet)
+
+#7 PYTHON da o‘zingizning “Guruhingiz nomini” ekranga chiqaring.
+guruh = "PRIP_21-UB"
+print(guruh)
+
+#8 PYTHON da ushbu gapni “O’zbekiston kelajagi buyuk davlat!” ekranga chiqaring.
+davlat = "O`zbekiston kelajagi buyuk davlat!"
+print(davlat)
+
+#8 PYTHON da ushbu gapni “Men PYTHON dasturlash tilini o‘rganmoq- chiman” ekranga chiqaring. 
+dastur = "Men Python dasturlash tilini o`rganmoq-chiman"
+print(dastur)
+
+#9 O‘zbekinton Respublikasi madhiyasining birinchi to‘rtligini ekranga chiqaring.
+madhiya = """Serquyosh, hur oʻlkam, elga baxt, najot,
+Sen oʻzing doʻstlarga yoʻldosh, mehribon!
+Yashnagay to abad ilmu fan, ijod,
+Shuhrating porlasin toki bor jahon!"""
+print(madhiya)
+
+#10 O‘zbekinton Respubliksi madhiyasining ikkinchi to‘rtligini ekranga chiqaring.
+madhiya = """Oltin bu vodiylar — jon Oʻzbekiston,
+Ajdodlar mardona ruhi senga yor!
+Ulugʻ xalq qudrati joʻsh urgan zamon,
+Olamni mahliyo aylagan diyor!"""
+print(madhiya)
+
+#11 O‘zbekinton Respubliksi madhiyasining uchinchi to‘rtligini
+madhiya = """Bagʻri keng oʻzbekning oʻchmas iymoni,
+Erkin, yosh avlodlar senga zoʻr qanot!
+Istiqlol mashʼali, tinchlik posboni,
+Haqsevar, ona yurt, mangu boʻl obod!"""
+print(madhiya)
+
+
+
+
+
